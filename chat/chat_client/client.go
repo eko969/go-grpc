92e019bac0eaeed08798f6ff8b096ea21e33b3dc
eko969 <ekonurrohman969@gmail.com>
2021-07-02 08:40:50 +0700

    Added client connection

--- chat/chat_client/client.go
+++ chat/chat_client/client.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/eko9x9/go-grpc/chat/chatpb"
+	"google.golang.org/grpc"
+)
+
 func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Could't connect server %v", err)
+	}
+
+	defer conn.Close()
+
+	c := chatpb.NewChatServiceClient(conn)
+
+	message := chatpb.Message{
+		Body: "Hello from the client",
+	}
+	resp, err := c.SayHello(context.Background(), &message)
+
+	if err != nil {
+		log.Fatalf("Error when calling SayHello %v", err)
+	}
+
+	log.Printf("Respon from the server %v", resp)
 
 }
